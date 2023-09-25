99647f045f2e725f7e23b2068122f186ec65d367
MarioNadal <220056@iessanalberto.com>
2023-09-25 09:16:02 +0200

    Ejercicio BackUp, no copia bien 25/09/2023

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.io.IOException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
